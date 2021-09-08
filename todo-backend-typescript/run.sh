c37e03c530625e82ce659503a5c33c156c26bbc5
Irina Southwell <irinasouthwell@gmail.com>
2021-09-09 08:43:16 +1200

    Update run.sh

--- todo-backend-typescript/run.sh
+++ todo-backend-typescript/run.sh
@@ -2,3 +2,4 @@
 export FEATUREHUB_ACCEPT_BAGGAGE=true
 export FEATUREHUB_EDGE_URL=http://localhost:8085/
 export FEATUREHUB_CLIENT_API_KEY=default/806345f2-3dea-4f61-9859-af7cc297bd3a/a3bXISQ9kFmLjsA6pONOINZDloWE4i*9HxvMfGOi1LpS4511lgk
+npm run start
