065337028106744187cbc84e02de93e0427daf3d
Irina Southwell <irinasouthwell@gmail.com>
2021-09-08 21:47:29 +1200

    remove csharp example as it is now in it's own SDK repo

--- todo-backend-typescript/run.sh
+++ todo-backend-typescript/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export FEATUREHUB_ACCEPT_BAGGAGE=true
-export FEATUREHUB_EDGE_URL=http://localhost:8085/
-export FEATUREHUB_CLIENT_API_KEY=default/806345f2-3dea-4f61-9859-af7cc297bd3a/a3bXISQ9kFmLjsA6pONOINZDloWE4i*9HxvMfGOi1LpS4511lgk
+export FEATUREHUB_EDGE_URL=http://localhost:8553/
+export FEATUREHUB_CLIENT_API_KEY=default/274dee2f-a81a-4040-9571-0c6633d01c82/N6qfWnklYQiObJ5TmB4ipF6xpEWdoX*ZjjVE2WKRmLf4UoMYBvI
 npm run start
