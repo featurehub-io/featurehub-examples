0c1fa1b5e14c19ccc57e10d22719fe8e09564dd5
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-10-15 00:33:42 +0530

    docs(angular) : improve angular example (#46)

--- todo-angular/angular-featurehub-app/src/app/app.module.ts
+++ todo-angular/angular-featurehub-app/src/app/app.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureToken, FEATURE_FLAG_TOKEN } from './feature-token';
+import { FeatureHubFlagService } from './featurehub-service';
+import { environment } from '../environments/environment';
+
+
+const featureToken: FeatureToken = {
+  apiKey: environment.apiKey, // api key
+  host: environment.host, // host 
+};
+
+function loadFeatureHub(featuerHub: FeatureHubFlagService) {
+  return () => featuerHub.initFeatureHub();
+}
+
 
 @NgModule({
   declarations: [
@@ -12,7 +26,18 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: FEATURE_FLAG_TOKEN,
+      useValue: featureToken,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadFeatureHub,
+      deps: [FeatureHubFlagService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
